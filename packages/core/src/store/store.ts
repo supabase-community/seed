7787a941afb31a79df84cca0ce22475ed4ce0f78
jgoux <hi@jgoux.dev>
2024-02-05 18:44:33 +0100

    use ModelData everywhere

--- packages/core/src/store/store.ts
+++ packages/core/src/store/store.ts
@@ -2,6 +2,6 @@ import { type ModelData } from "../plan/types.js";
 
 export interface Store {
   _store: Record<string, Array<ModelData>>;
-  add(model: string, value: unknown): void;
+  add(model: string, value: ModelData): void;
   toSQL(): Array<string>;
 }
