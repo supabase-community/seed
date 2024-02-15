c8df74ce56290c4a00f638d02f6334668701ef1c
jgoux <hi@jgoux.dev>
2024-02-15 11:01:08 +0100

    refactor

--- packages/seed/src/cli/commands/introspect/introspectHandler.ts
+++ packages/seed/src/cli/commands/introspect/introspectHandler.ts
@@ -4,19 +4,7 @@ export async function introspectHandler(args: { connectionString: string }) {
   const protocol = new URL(args.connectionString).protocol.slice(0, -1);
 
   if (protocol === "postgres" || protocol === "postgresql") {
-    await assertPackage("pg");
-    const { Client } = await import("pg");
-    const client = new Client({ connectionString: args.connectionString });
-    await client.connect();
-    const { drizzle } = await import("drizzle-orm/node-postgres");
-    const db = drizzle(client);
-    const { getDatamodel } = await import("#dialects/postgres/dataModel.js");
-    try {
-      const dataModel = await getDatamodel(db);
-      await setDataModelConfig(dataModel);
-    } finally {
-      await client.end();
-    }
+    await pgIntrospect(args.connectionString);
   } else if (protocol === "mysql") {
     throw new Error(`Not implemented yet`);
   } else if (protocol === "file") {
@@ -25,6 +13,22 @@ export async function introspectHandler(args: { connectionString: string }) {
     throw new Error(`Not implemented yet`);
   } else {
     throw new Error(`Unsupported protocol: ${protocol}`);
+  }
+}
+
+async function pgIntrospect(connectionString: string) {
+  await assertPackage("pg");
+  const { Client } = await import("pg");
+  const client = new Client({ connectionString });
+  await client.connect();
+  const { drizzle } = await import("drizzle-orm/node-postgres");
+  const db = drizzle(client);
+  const { getDatamodel } = await import("#dialects/postgres/dataModel.js");
+  try {
+    const dataModel = await getDatamodel(db);
+    await setDataModelConfig(dataModel);
+  } finally {
+    await client.end();
   }
 }
 
