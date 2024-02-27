f882f4a26308b84faf59b9df01839284754eb1aa
avallete <andrew@snaplet.dev>
2024-02-27 09:55:08 +0100

    fix: use DrizzleDbClient

--- packages/seed/src/dialects/postgres/dataModel.ts
+++ packages/seed/src/dialects/postgres/dataModel.ts
@@ -1,11 +1,9 @@
+import { type DrizzleDbClient } from "#core/adapters.js";
 import { type DataModel } from "#core/dataModel/types.js";
-import { type DrizzleORMPgClient } from "./adapters.js";
 import { introspectDatabase } from "./introspect/introspectDatabase.js";
 import { introspectionToDataModel } from "./introspect/introspectionToDataModel.js";
 
-export async function getDatamodel(
-  _db: DrizzleORMPgClient,
-): Promise<DataModel> {
+export async function getDatamodel(_db: DrizzleDbClient): Promise<DataModel> {
   const introspection = await introspectDatabase(_db);
   const dataModel = introspectionToDataModel(introspection);
   return dataModel;
