7b352063607bed59ce4ecb6b7188547b6b084ede
jgoux <hi@jgoux.dev>
2024-03-13 16:12:19 +0100

    feat(cli): setup command

---
+++ packages/seed/src/cli/commands/generate/generateHandler.ts
@@ -0,0 +1,47 @@
+import { type CodegenContext, generateAssets } from "#core/codegen/codegen.js";
+import { getDataModel } from "#core/dataModel/dataModel.js";
+import { getDialect } from "#core/dialect/getDialect.js";
+import { getFingerprint } from "#core/fingerprint/fingerprint.js";
+import { type TableShapePredictions } from "#trpc/shapes.js";
+import { spinner } from "../../lib/output.js";
+import { fetchShapeExamples } from "./fetchShapeExamples.js";
+import { fetchShapePredictions } from "./fetchShapePredictions.js";
+
+export async function generateHandler(args: { output?: string }) {
+  const context = await computeCodegenContext({ outputDir: args.output });
+  await generateAssets(context);
+  console.log("Done!");
+}
+
+async function computeCodegenContext(props: {
+  outputDir: string | undefined;
+}): Promise<CodegenContext> {
+  const { outputDir } = props;
+
+  const dataModel = await getDataModel();
+  const dialect = await getDialect(dataModel.dialect);
+  let shapePredictions: Array<TableShapePredictions> = [];
+  let shapeExamples: Array<{ examples: Array<string>; shape: string }> = [];
+
+  if (!process.env["SNAPLET_DISABLE_SHAPE_PREDICTION"]) {
+    spinner.start("Predicting data labels...");
+    shapePredictions = await fetchShapePredictions({
+      determineShapeFromType: dialect.determineShapeFromType,
+      dataModel,
+    });
+    spinner.succeed();
+
+    spinner.start("Loading label examples...");
+    shapeExamples = await fetchShapeExamples(shapePredictions);
+    spinner.succeed();
+  }
+
+  return {
+    fingerprint: await getFingerprint(),
+    dataModel,
+    outputDir,
+    shapePredictions,
+    shapeExamples,
+    dialect,
+  };
+}
