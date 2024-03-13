1db3237d6887aff2d9a2969c2454dba650ab6afd
Justin van der Merwe <justnvdm@gmail.com>
2024-03-13 16:49:48 +0200

    Add timeout

--- packages/seed/e2e/e2e.postgres.test.ts
+++ packages/seed/e2e/e2e.postgres.test.ts
@@ -4,11 +4,13 @@ import { setupProject } from "#test/setupProject.js";
 
 const adapter = await adapters.postgres();
 
-describe(`e2e: postgres-specific`, () => {
-  test("works with multiple schemas", async () => {
-    const { db } = await setupProject({
-      adapter,
-      databaseSchema: `
+describe(
+  `e2e: postgres-specific`,
+  () => {
+    test("works with multiple schemas", async () => {
+      const { db } = await setupProject({
+        adapter,
+        databaseSchema: `
           CREATE SCHEMA "other";
           CREATE TABLE "public"."User" (
             "id" uuid not null primary key
@@ -17,8 +19,8 @@ describe(`e2e: postgres-specific`, () => {
             "id" uuid not null primary key
           );
         `,
-      snapletConfig: null,
-      seedScript: `
+        snapletConfig: null,
+        seedScript: `
           import { createSeedClient } from '#seed'
 
           const seed = await createSeedClient()
@@ -26,31 +28,37 @@ describe(`e2e: postgres-specific`, () => {
           await seed.users((x) => x(2))
           await seed.posts((x) => x(2))
         `,
+      });
+
+      expect((await db.query('select * from "User"')).length).toEqual(2);
+
+      expect((await db.query('select * from "other"."Post"')).length).toEqual(
+        2,
+      );
     });
 
-    expect((await db.query('select * from "User"')).length).toEqual(2);
-
-    expect((await db.query('select * from "other"."Post"')).length).toEqual(2);
-  });
-
-  test("work with citext pg type", async () => {
-    const { db } = await setupProject({
-      adapter,
-      databaseSchema: `
+    test("work with citext pg type", async () => {
+      const { db } = await setupProject({
+        adapter,
+        databaseSchema: `
           CREATE EXTENSION citext;
           CREATE TABLE "user" (
             "id" SERIAL PRIMARY KEY,
             "email" citext NOT NULL
           );
         `,
-      seedScript: `
+        seedScript: `
           import { createSeedClient } from "#seed"
           const seed = await createSeedClient()
           await seed.users((x) => x(2))
         `,
-    });
+      });
 
-    // Check if the tables have been populated with the correct number of entries
-    expect((await db.query('SELECT * FROM "user"')).length).toEqual(2);
-  });
-});
+      // Check if the tables have been populated with the correct number of entries
+      expect((await db.query('SELECT * FROM "user"')).length).toEqual(2);
+    });
+  },
+  {
+    timeout: 45000,
+  },
+);
