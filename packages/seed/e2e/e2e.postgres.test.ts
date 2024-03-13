a1df40918aca0eef88dea18cf42eefb296128c3a
Justin van der Merwe <justnvdm@gmail.com>
2024-03-14 00:19:11 +0200

    tests: Port over e2e: @snaplet/seed programmatic API (e2e.api.test.ts)

--- packages/seed/e2e/e2e.postgres.test.ts
+++ packages/seed/e2e/e2e.postgres.test.ts
@@ -57,6 +57,208 @@ describe.concurrent(
       // Check if the tables have been populated with the correct number of entries
       expect((await db.query('SELECT * FROM "user"')).length).toEqual(2);
     });
+
+    describe("$resetDatabase", () => {
+      test("should reset all schemas and tables per default", async () => {
+        const seedScript = `
+        import { createSeedClient } from '#seed'
+        const seed = await createSeedClient()
+        await seed.$resetDatabase()
+        await seed.teams((x) => x(2, {
+          players: (x) => x(3)
+        }));
+        await seed.games((x) => x(3));
+      `;
+        const { db, runSeedScript } = await setupProject({
+          adapter,
+          databaseSchema: `
+            CREATE SCHEMA IF NOT EXISTS hdb_catalog;
+            CREATE TABLE hdb_catalog."SystemSettings" (
+              "settingKey" VARCHAR(255) PRIMARY KEY,
+              "settingValue" TEXT NOT NULL
+            );
+
+            CREATE TABLE hdb_catalog."AuditLog" (
+              "logId" SERIAL PRIMARY KEY,
+              "action" TEXT NOT NULL,
+              "timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+              "userId" INTEGER
+            );
+
+            INSERT INTO hdb_catalog."SystemSettings" ("settingKey", "settingValue") VALUES
+            ('theme', 'dark'),
+            ('retryInterval', '30');
+
+            INSERT INTO hdb_catalog."AuditLog" ("action", "userId") VALUES
+            ('System Start', NULL),
+            ('Initial Configuration', NULL);
+
+            CREATE TABLE "Team" (
+              "id" SERIAL PRIMARY KEY
+            );
+            CREATE TABLE "Player" (
+              "id" BIGSERIAL PRIMARY KEY,
+              "teamId" integer NOT NULL REFERENCES "Team"("id"),
+              "name" text NOT NULL
+            );
+            CREATE TABLE "Game" (
+              "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+            );
+          `,
+          seedScript,
+        });
+        expect((await db.query('SELECT * FROM "Player"')).length).toEqual(6);
+        expect(
+          (await db.query('SELECT * FROM hdb_catalog."AuditLog"')).length,
+        ).toEqual(0);
+        expect(
+          (await db.query('SELECT * FROM hdb_catalog."SystemSettings"')).length,
+        ).toEqual(0);
+        // Should be able to re-run the seed script again thanks to the $resetDatabase
+        await runSeedScript(seedScript);
+        expect((await db.query('SELECT * FROM "Player"')).length).toEqual(6);
+      });
+
+      test("should not reset config excluded schema", async () => {
+        const snapletConfig = `
+        import { defineConfig } from "@snaplet/seed/config";
+
+        export default defineConfig({
+          select: {
+            'hdb_catalog.*': false,
+          },
+        })
+  `;
+        const seedScript = `
+        import { createSeedClient } from "#seed"
+        const seed = await createSeedClient()
+        await seed.$resetDatabase()
+        await seed.teams((x) => x(2, {
+          players: (x) => x(3)
+        }));
+        await seed.games((x) => x(3));
+      `;
+        const { db, runSeedScript } = await setupProject({
+          adapter,
+          databaseSchema: `
+            CREATE SCHEMA IF NOT EXISTS hdb_catalog;
+            CREATE TABLE hdb_catalog."SystemSettings" (
+              "settingKey" VARCHAR(255) PRIMARY KEY,
+              "settingValue" TEXT NOT NULL
+            );
+
+            CREATE TABLE hdb_catalog."AuditLog" (
+              "logId" SERIAL PRIMARY KEY,
+              "action" TEXT NOT NULL,
+              "timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+              "userId" INTEGER
+            );
+
+            INSERT INTO hdb_catalog."SystemSettings" ("settingKey", "settingValue") VALUES
+            ('theme', 'dark'),
+            ('retryInterval', '30');
+
+            INSERT INTO hdb_catalog."AuditLog" ("action", "userId") VALUES
+            ('System Start', NULL),
+            ('Initial Configuration', NULL);
+
+            CREATE TABLE "Team" (
+              "id" SERIAL PRIMARY KEY
+            );
+            CREATE TABLE "Player" (
+              "id" BIGSERIAL PRIMARY KEY,
+              "teamId" integer NOT NULL REFERENCES "Team"("id"),
+              "name" text NOT NULL
+            );
+            CREATE TABLE "Game" (
+              "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+            );
+          `,
+          snapletConfig,
+          seedScript,
+        });
+        expect((await db.query('SELECT * FROM "Player"')).length).toEqual(6);
+        expect(
+          (await db.query('SELECT * FROM hdb_catalog."AuditLog"')).length,
+        ).toEqual(2);
+        expect(
+          (await db.query('SELECT * FROM hdb_catalog."SystemSettings"')).length,
+        ).toEqual(2);
+        // Should be able to re-run the seed script again thanks to the $resetDatabase
+        await runSeedScript(seedScript);
+        expect((await db.query('SELECT * FROM "Player"')).length).toEqual(6);
+      });
+
+      test("should not reset config excluded table", async () => {
+        const snapletConfig = `
+        import { defineConfig } from "@snaplet/seed/config";
+
+        export default defineConfig({
+          select: {
+            "hdb_catalog.SystemSettings": false,
+          },
+        })
+  `;
+        const seedScript = `
+        import { createSeedClient } from "#seed"
+        const seed = await createSeedClient()
+        await seed.$resetDatabase()
+        await seed.teams((x) => x(2, {
+          players: (x) => x(3)
+        }));
+        await seed.games((x) => x(3));
+      `;
+        const { db, runSeedScript } = await setupProject({
+          adapter,
+          databaseSchema: `
+            CREATE SCHEMA IF NOT EXISTS hdb_catalog;
+            CREATE TABLE hdb_catalog."SystemSettings" (
+              "settingKey" VARCHAR(255) PRIMARY KEY,
+              "settingValue" TEXT NOT NULL
+            );
+
+            CREATE TABLE hdb_catalog."AuditLog" (
+              "logId" SERIAL PRIMARY KEY,
+              "action" TEXT NOT NULL,
+              "timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+              "userId" INTEGER
+            );
+
+            INSERT INTO hdb_catalog."SystemSettings" ("settingKey", "settingValue") VALUES
+            ('theme', 'dark'),
+            ('retryInterval', '30');
+
+            INSERT INTO hdb_catalog."AuditLog" ("action", "userId") VALUES
+            ('System Start', NULL),
+            ('Initial Configuration', NULL);
+
+            CREATE TABLE "Team" (
+              "id" SERIAL PRIMARY KEY
+            );
+            CREATE TABLE "Player" (
+              "id" BIGSERIAL PRIMARY KEY,
+              "teamId" integer NOT NULL REFERENCES "Team"("id"),
+              "name" text NOT NULL
+            );
+            CREATE TABLE "Game" (
+              "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+            );
+          `,
+          snapletConfig,
+          seedScript,
+        });
+        expect((await db.query('SELECT * FROM "Player"')).length).toEqual(6);
+        expect(
+          (await db.query('SELECT * FROM hdb_catalog."AuditLog"')).length,
+        ).toEqual(0);
+        expect(
+          (await db.query('SELECT * FROM hdb_catalog."SystemSettings"')).length,
+        ).toEqual(2);
+        // Should be able to re-run the seed script again thanks to the $resetDatabase
+        await runSeedScript(seedScript);
+        expect((await db.query('SELECT * FROM "Player"')).length).toEqual(6);
+      });
+    });
   },
   {
     timeout: 45000,
