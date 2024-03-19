62af0b2779afbdbffe7d1177d62a3d4a1aea4f08
Justin van der Merwe <justnvdm@gmail.com>
2024-03-19 13:26:50 +0200

    IS_PRODUCTION based on src

--- packages/seed/src/config/constants.ts
+++ packages/seed/src/config/constants.ts
@@ -1,9 +1,13 @@
-const PRODUCTION_API_URL = "https://api.snaplet.dev/cli";
+import { basename, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const SNAPLET_API_URL =
-  process.env["SNAPLET_API_URL"] ?? PRODUCTION_API_URL;
+  process.env["SNAPLET_API_URL"] ?? "https://api.snaplet.dev/cli";
 
 export const SNAPLET_APP_URL =
   process.env["SNAPLET_APP_URL"] ?? "https://app.snaplet.dev";
 
-export const IS_PRODUCTION = SNAPLET_API_URL === PRODUCTION_API_URL;
+export const IS_PRODUCTION = basename(dirname(__dirname)) !== "src";
