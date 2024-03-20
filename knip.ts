fda5a38ad19c8ec47bd7184b12ed5d229ecaf696
jgoux <hi@jgoux.dev>
2024-03-20 11:47:19 +0100

    ignore index

--- knip.ts
+++ knip.ts
@@ -1,13 +1,18 @@
 import { type KnipConfig } from "knip";
 import { readFileSync } from "node:fs";
 
-const seedPackageJson = JSON.parse(
-  readFileSync("./packages/seed/package.json", "utf8"),
-) as { peerDependenciesMeta: Record<string, { optional: boolean }> };
+const optionalPeerDeps = Object.keys(
+  (
+    JSON.parse(readFileSync("./packages/seed/package.json", "utf8")) as {
+      peerDependenciesMeta: Record<string, { optional: boolean }>;
+    }
+  ).peerDependenciesMeta,
+);
 
 const config: KnipConfig = {
   ignoreBinaries: ["nix"],
-  ignoreDependencies: Object.keys(seedPackageJson.peerDependenciesMeta),
+  ignoreDependencies: optionalPeerDeps,
+  ignore: ["./packages/seed/src/index.ts"],
 };
 
 export default config;
