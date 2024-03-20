fc05591670de1028d236b0489ad74c288a8b0289
jgoux <hi@jgoux.dev>
2024-03-20 08:11:29 +0100

    fix more issues

--- knip.ts
+++ knip.ts
@@ -1,7 +1,13 @@
 import { type KnipConfig } from "knip";
+import { readFileSync } from "node:fs";
+
+const seedPackageJson = JSON.parse(
+  readFileSync("./packages/seed/package.json", "utf8"),
+) as { peerDependenciesMeta: Record<string, { optional: boolean }> };
 
 const config: KnipConfig = {
   ignoreBinaries: ["nix"],
+  ignoreDependencies: Object.keys(seedPackageJson.peerDependenciesMeta),
 };
 
 export default config;
