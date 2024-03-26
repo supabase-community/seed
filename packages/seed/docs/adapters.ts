42d3f74e113e007246f3275666a992e9a993cab3
jgoux <hi@jgoux.dev>
2024-03-26 16:28:15 +0100

    adapters

--- packages/seed/docs/adapters.ts
+++ packages/seed/docs/adapters.ts
@@ -1,9 +1,67 @@
-import { Project } from "ts-morph";
+import { globby } from "globby";
+import { basename, join } from "node:path";
+import { Project, SyntaxKind, type VariableDeclaration } from "ts-morph";
 
 /**
- * This script generates a description of all the commands available in the CLI from our source code.
+ * This script generates a description of all the adapters available from our source code.
  */
+
+interface Adapter {
+  id: string;
+  name: string;
+  packageName: string;
+}
+
+const adapters: Array<Adapter> = [];
+
+const adaptersPaths = (
+  await globby("src/adapters/*", { onlyDirectories: true })
+).map((p) => join(p, `${basename(p)}.ts`));
 
 const project = new Project();
 
-project.addSourceFilesAtPaths("src/cli/commands/*/*Command.ts");
+project.addSourceFilesAtPaths(adaptersPaths);
+
+for (const sourceFile of project.getSourceFiles()) {
+  const exportedDeclarations = Array.from(
+    sourceFile.getExportedDeclarations().values(),
+  ).flat();
+  const adapterDefinition = exportedDeclarations.find(
+    (ed) =>
+      ed.isKind(SyntaxKind.VariableDeclaration) &&
+      ed.getName().endsWith("Adapter"),
+  ) as VariableDeclaration | undefined;
+
+  if (!adapterDefinition) {
+    throw new Error(
+      `Adapter definition not found for file ${sourceFile.getFilePath()}`,
+    );
+  }
+
+  const objectDefinition = adapterDefinition.getFirstDescendantByKindOrThrow(
+    SyntaxKind.ObjectLiteralExpression,
+  );
+
+  const adapterId = objectDefinition
+    .getPropertyOrThrow("id")
+    .getFirstDescendantByKindOrThrow(SyntaxKind.StringLiteral)
+    .getLiteralValue();
+
+  const adapterName = objectDefinition
+    .getPropertyOrThrow("name")
+    .getFirstDescendantByKindOrThrow(SyntaxKind.StringLiteral)
+    .getLiteralValue();
+
+  const adapterPackageName = objectDefinition
+    .getPropertyOrThrow("packageName")
+    .getFirstDescendantByKindOrThrow(SyntaxKind.StringLiteral)
+    .getLiteralValue();
+
+  adapters.push({
+    id: adapterId,
+    name: adapterName,
+    packageName: adapterPackageName,
+  });
+}
+
+console.log(JSON.stringify(adapters, null, 2));
