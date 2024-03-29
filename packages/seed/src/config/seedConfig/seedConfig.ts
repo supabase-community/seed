867a6d707f9734f22cee759ee04266b88b8ad2c1
jgoux <hi@jgoux.dev>
2024-03-29 10:37:01 +0100

    chore: port docs to defineConfig

--- packages/seed/src/config/seedConfig/seedConfig.ts
+++ packages/seed/src/config/seedConfig/seedConfig.ts
@@ -18,7 +18,98 @@ const configSchema = z.object({
   // TODO: add "introspect" config here to enable virtual constraints user defined setup
 });
 
-export type SeedConfig = z.infer<typeof configSchema>;
+type SeedConfigInferred = z.infer<typeof configSchema>;
+
+export interface SeedConfig {
+  /**
+   * The database adapter to use.
+   *
+   * @example
+   * ```ts seed.config.ts
+   * import { SeedPostgres } from "@snaplet/seed/adapter-postgres";
+   * import { defineConfig } from "@snaplet/seed/config";
+   * import postgres from "postgres";
+   *
+   * export default defineConfig({
+   *   adapter: () => {
+   *     const client = postgres(process.env.DATABASE_URL);
+   *     return new SeedPostgres(client);
+   *   },
+   * });
+   * ```
+   *
+   * To learn more about the available adapters, see the [Adapters](https://docs.snaplet.dev/seed/reference/adapters) reference.
+   */
+  adapter: SeedConfigInferred["adapter"];
+  alias?: {
+    /**
+     * Apply a global renaming strategy to all tables and columns in the generated Seed Client.
+     *
+     * When `true`, a default strategy is applied:
+     *
+     * - **Model names:** pluralized and camelCased.
+     * - **Scalar field names:** camelCased.
+     * - **Parent field names (one to one relationships):** singularized and camelCased.
+     * - **Child field names (one to many relationships):** pluralized and camelCased.
+     * - We also support prefix extraction and opposite baseName for foreign keys inspired by [PostGraphile](https://github.com/graphile/pg-simplify-inflector#naming-your-foreign-key-fields).
+     *
+     * @example
+     * ```ts seed.client.ts
+     * import { defineConfig } from "@snaplet/seed/config";
+     *
+     * export default defineConfig({
+     *   alias: {
+     *     inflection: true,
+     *   },
+     * });
+     * ```
+     */
+    inflection?: NonNullable<SeedConfigInferred["alias"]>["inflection"];
+    /**
+     * Rename specific tables and columns in the generated Seed Client.
+     * This option is useful for resolving renaming conflicts that can arise when using `alias.inflection`.
+     *
+     * @example
+     * ```ts seed.client.ts
+     * import { defineConfig } from "@snaplet/seed/config";
+     *
+     * export default defineConfig({
+     *   alias: {
+     *     override: {
+     *       Book: {
+     *         name: "books",
+     *         fields: {
+     *           User: "author",
+     *           published_at: "publishedAt",
+     *         },
+     *       },
+     *     },
+     *   },
+     * });
+     * ```
+     */
+    override?: NonNullable<SeedConfigInferred["alias"]>["override"];
+  };
+  fingerprint?: SeedConfigInferred["fingerprint"];
+  /**
+   * Exclude tables from the generated Seed Client.
+   * For excluding multiple tables at once, you can use a wildcard character `*` at the end of the table name.
+   *
+   * @example
+   * ```ts seed.client.ts
+   * import { defineConfig } from "@snaplet/seed/config";
+   *
+   * export default defineConfig({
+   *   select: {
+   *     "archive*": false,
+   *     "access_logs": false,
+   *     "auth.*": false,
+   *   },
+   * });
+   * ```
+   */
+  select?: SeedConfigInferred["select"];
+}
 
 export async function getSeedConfig() {
   const { config } = await loadConfig({
