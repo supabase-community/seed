c6315cccaa01e40e0f37df20f8646ffd7e8a97fd
jgoux <hi@jgoux.dev>
2024-03-29 18:11:10 +0100

    parallelize tests

--- packages/seed/e2e/e2e.docs.test.ts
+++ packages/seed/e2e/e2e.docs.test.ts
@@ -11,9 +11,9 @@ describe.each(adapterEntries)(
     concurrent: true,
   },
   (dialect, adapter) => {
-    test("classical relationships examples", async () => {
-      // Ensure behaviour is consistent with the documentation
-      // https://github.com/snaplet/examples/pull/8
+    // Ensure behaviour is consistent with the documentation
+    // https://github.com/snaplet/examples/pull/8
+    describe("classical relationships examples", () => {
       const schema: DialectRecordWithDefault = {
         default: `
             CREATE TABLE "User" (
@@ -71,92 +71,130 @@ describe.each(adapterEntries)(
             );`,
       };
 
-      const one_to_many_script = `
-        import { createSeedClient } from '#seed'
-        const seed = await createSeedClient()
-        // Clear all tables
-        await seed.$resetDatabase();
+      test("one to many", async () => {
+        const script = `
+          import { createSeedClient } from '#seed'
+          const seed = await createSeedClient()
+          // Clear all tables
+          await seed.$resetDatabase();
 
-        // Create 5 users, each potentially having up to 5 posts
-        // Posts at this stage do not have associated tags
-        await seed.User((x) => x(5, {
-            Post: (x) => x({min: 0, max: 5})
-        }))`;
-      const many_to_many_script = `
-        import { createSeedClient } from '#seed'
-        const seed = await createSeedClient()
-        // Clear all tables
-        await seed.$resetDatabase();
+          // Create 5 users, each potentially having up to 5 posts
+          // Posts at this stage do not have associated tags
+          await seed.User((x) => x(5, {
+              Post: (x) => x({min: 0, max: 5})
+          }))
+        `;
 
-        // Create 5 users
-        await seed.User(
-            (x) => x(5, {
-            // Each can have up to 5 posts
-            Post: (x) => x({min: 0, max: 5},
-                () => ({
-                    // Each post can be associated with up to 3 tags
-                    PostTags: (x) => x({min: 0, max: 3})
-                })
-            )}))`;
-      const many_to_many_pool_script = `
-        import { createSeedClient } from '#seed'
-        const seed = await createSeedClient()
-        // Clear all tables
-        await seed.$resetDatabase();
+        const { db, runSeedScript } = await setupProject({
+          adapter,
+          databaseSchema: schema[dialect] ?? schema.default,
+          seedConfig: (connectionString) =>
+            adapter.generateSeedConfig(connectionString, {
+              alias: "{ inflection: false }",
+            }),
+        });
 
-        // Initially, create a pool of 5 tags for post association
-        const { Tag } = await seed.Tag((x) => x(5))
+        await runSeedScript(script);
 
-        // Create 5 users
-        await seed.User(
-            (x) => x(5, {
-            // Each can have up to 5 posts
-            Post: (x) => x({min: 0, max: 5},
-                () => ({
-                    // Each post can be associated with up to 3 tags
-                    PostTags: (x) => x({min: 0, max: 3})
-                })
-            )}),
-            {
-            // Link the posts to the pre-created tags
-            connect: { Tag }
-        })`;
-      const { db, runSeedScript } = await setupProject({
-        adapter,
-        databaseSchema: schema[dialect] ?? schema.default,
-        seedConfig: (connectionString) =>
-          adapter.generateSeedConfig(connectionString, {
-            alias: "{ inflection: false }",
-          }),
+        const users = await db.query('select * from "User"');
+        const posts = await db.query('select * from "Post"');
+        const postTags = await db.query('select * from "PostTags"');
+        const tags = await db.query('select * from "Tag"');
+        expect(users.length).toEqual(5);
+        expect(posts.length).toEqual(17);
+        expect(postTags.length).toEqual(0);
+        expect(tags.length).toEqual(0);
       });
 
-      await runSeedScript(one_to_many_script);
-      const users = await db.query('select * from "User"');
-      const posts = await db.query('select * from "Post"');
-      const postTags = await db.query('select * from "PostTags"');
-      const tags = await db.query('select * from "Tag"');
-      expect(users.length).toEqual(5);
-      expect(posts.length).toEqual(17);
-      expect(postTags.length).toEqual(0);
-      expect(tags.length).toEqual(0);
-      await runSeedScript(many_to_many_script);
-      const users2 = await db.query('select * from "User"');
-      const posts2 = await db.query('select * from "Post"');
-      const postTags2 = await db.query('select * from "PostTags"');
-      const tags2 = await db.query('select * from "Tag"');
-      expect(users2.length).toEqual(5);
-      expect(posts2.length).toEqual(17);
-      expect(postTags2.length).toEqual(30);
-      expect(tags2.length).toEqual(30);
-      await runSeedScript(many_to_many_pool_script);
-      const users3 = await db.query('select * from "User"');
-      const posts3 = await db.query('select * from "Post"');
-      const postTags3 = await db.query('select * from "PostTags"');
-      const tags3 = await db.query('select * from "Tag"');
-      expect(users3.length).toEqual(5);
-      expect(posts3.length).toEqual(17);
-      expect(postTags3.length).toEqual(30);
-      expect(tags3.length).toEqual(5);
+      test("many to many", async () => {
+        const script = `
+          import { createSeedClient } from '#seed'
+          const seed = await createSeedClient()
+          // Clear all tables
+          await seed.$resetDatabase();
+
+          // Create 5 users
+          await seed.User(
+              (x) => x(5, {
+              // Each can have up to 5 posts
+              Post: (x) => x({min: 0, max: 5},
+                  () => ({
+                      // Each post can be associated with up to 3 tags
+                      PostTags: (x) => x({min: 0, max: 3})
+                  })
+              )}
+            )
+          )
+        `;
+
+        const { db, runSeedScript } = await setupProject({
+          adapter,
+          databaseSchema: schema[dialect] ?? schema.default,
+          seedConfig: (connectionString) =>
+            adapter.generateSeedConfig(connectionString, {
+              alias: "{ inflection: false }",
+            }),
+        });
+
+        await runSeedScript(script);
+
+        const users = await db.query('select * from "User"');
+        const posts = await db.query('select * from "Post"');
+        const postTags = await db.query('select * from "PostTags"');
+        const tags = await db.query('select * from "Tag"');
+        expect(users.length).toEqual(5);
+        expect(posts.length).toEqual(17);
+        expect(postTags.length).toEqual(30);
+        expect(tags.length).toEqual(30);
+      });
+
+      test("many to many with pool", async () => {
+        const script = `
+          import { createSeedClient } from '#seed'
+          const seed = await createSeedClient()
+          // Clear all tables
+          await seed.$resetDatabase();
+
+          // Initially, create a pool of 5 tags for post association
+          const { Tag } = await seed.Tag((x) => x(5))
+
+          // Create 5 users
+          await seed.User(
+            (x) => x(5, {
+            // Each can have up to 5 posts
+            Post: (x) => x({min: 0, max: 5},
+              () => ({
+                // Each post can be associated with up to 3 tags
+                PostTags: (x) => x({min: 0, max: 3})
+              })
+            )}),
+            {
+              // Link the posts to the pre-created tags
+              connect: { Tag }
+            }
+          )
+        `;
+
+        const { db, runSeedScript } = await setupProject({
+          adapter,
+          databaseSchema: schema[dialect] ?? schema.default,
+          seedConfig: (connectionString) =>
+            adapter.generateSeedConfig(connectionString, {
+              alias: "{ inflection: false }",
+            }),
+        });
+
+        await runSeedScript(script);
+
+        const users = await db.query('select * from "User"');
+        const posts = await db.query('select * from "Post"');
+        const postTags = await db.query('select * from "PostTags"');
+        const tags = await db.query('select * from "Tag"');
+        expect(users.length).toEqual(5);
+        expect(posts.length).toEqual(17);
+        expect(postTags.length).toEqual(30);
+        expect(tags.length).toEqual(5);
+      });
     });
   },
 );
