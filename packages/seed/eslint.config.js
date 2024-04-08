ec54838ba9b459cd14787ea8c858e6dca117d97e
avallete <andrew@snaplet.dev>
2024-04-09 01:00:10 +0200

    fix: lint

--- packages/seed/eslint.config.js
+++ packages/seed/eslint.config.js
@@ -1,3 +1,8 @@
 import { defineConfig, recommended } from "@snaplet/eslint-config";
 
-export default defineConfig(recommended);
+export default defineConfig([
+  ...recommended,
+  {
+    ignores: ["e2e/npm-install-test/**"],
+  },
+]);
