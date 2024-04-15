0f185b42631f93657840c4361029ebd995eede4c
Justin van der Merwe <justnvdm@gmail.com>
2024-04-15 16:33:43 +0200

    fix: Save user id when talking to API for identifying users on posthog
    There is some logic that was not ported over when switching to new seed repo (my bad): https://github.com/snaplet/snaplet/blob/bcf52cde1e6345fc25458f6ee2bab5d55304d51a/cli/src/lib/trpc.ts#L85
    
    In this code, whenever we make API requests in the CLI, we get a user id response header, and save it to the system config. This means any subsequent posthog events we send would be associated with that user id.
    
    Since we do not have this at the moment, we end up not being able to identify unique users if the user did not explicitly `login`, which is the case when users use `@snaplet/seed` on CI (with a `SNAPLET_ACCESS_TOKEN`).

--- packages/seed/src/trpc/client.ts
+++ packages/seed/src/trpc/client.ts
@@ -1,8 +1,27 @@
 import { createTRPCProxyClient, httpLink } from "@trpc/client";
 import { SNAPLET_API_URL } from "#config/constants.js";
-import { getSystemConfig } from "#config/systemConfig.js";
+import { getSystemConfig, updateSystemConfig } from "#config/systemConfig.js";
 import { getVersion } from "#core/version.js";
 import { type CLIRouter } from "./router.js";
+
+const httpFetch = globalThis.fetch;
+
+const fetch: typeof httpFetch = async (url, init) => {
+  const systemConfig = await getSystemConfig();
+
+  const result = await httpFetch(url, init);
+
+  const userId = result.headers.get("SNAPLET-USER-ID");
+
+  if (
+    typeof systemConfig.userId === "undefined" &&
+    typeof userId === "string"
+  ) {
+    await updateSystemConfig({ userId });
+  }
+
+  return result;
+};
 
 export const trpc = createTRPCProxyClient<CLIRouter>({
   links: [
@@ -12,6 +31,7 @@ export const trpc = createTRPCProxyClient<CLIRouter>({
         authorization: `Bearer ${await getAccessToken()}`,
         "user-agent": `Snaplet Seed / ${getVersion()}`,
       }),
+      fetch,
     }),
   ],
 });
