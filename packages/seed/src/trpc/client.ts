a7a883b15a54473cab0edbf9525f91c8aba70a34
Justin van der Merwe <justinvderm@gmail.com>
2024-04-15 18:04:17 +0200

    fix: Save user id when talking to API for identifying users on posthog (#112)
    There is some logic that was not ported over when switching to new seed repo (my bad): https://github.com/snaplet/snaplet/blob/bcf52cde1e6345fc25458f6ee2bab5d55304d51a/cli/src/lib/trpc.ts#L85
    
    In this code, whenever we make API requests in the CLI, we get a user id response header, and save it to the system config. This means any subsequent posthog events we send would be associated with that user id.
    
    Since we do not have this at the moment, we end up not being able to identify unique users if the user did not explicitly `login`, which is the case when users use `@snaplet/seed` on CI (with a `SNAPLET_ACCESS_TOKEN`).
    
    ## Note
    There are some limitations to how effectively this solution will work in the new seed repo, because of the `generate` command. More context in these threads:
    * https://discord.com/channels/788353076129038346/914821651017908234/1229426877320396880
    * https://discord.com/channels/788353076129038346/914821651017908234/1229413193571635340
    
    ## Loom with a test run to confirm the fix works
    https://www.loom.com/share/71b742074d2d47b5895a99d372b10f7d

--- packages/seed/src/trpc/client.ts
+++ packages/seed/src/trpc/client.ts
@@ -1,8 +1,45 @@
 import { createTRPCProxyClient, httpLink } from "@trpc/client";
 import { SNAPLET_API_URL } from "#config/constants.js";
-import { getSystemConfig } from "#config/systemConfig.js";
+import { getSystemConfig, updateSystemConfig } from "#config/systemConfig.js";
 import { getVersion } from "#core/version.js";
 import { type CLIRouter } from "./router.js";
+
+type FetchEsque = NonNullable<Parameters<typeof httpLink>[0]["fetch"]>;
+type FetchEsqueInput = URL | string;
+type FetchEsqueInit = Parameters<FetchEsque>[1];
+
+const httpFetch = globalThis.fetch;
+
+const fetch: FetchEsque = async (
+  input: FetchEsqueInput,
+  rawInit: FetchEsqueInit,
+) => {
+  const systemConfig = await getSystemConfig();
+
+  let init = undefined;
+
+  // context(justinvdm, 15 Apr 2024): There's a minor differences between the typedefs for
+  // trpc's `fetch` and native `fetch`
+  if (rawInit != null) {
+    init = {
+      ...rawInit,
+      signal: rawInit.signal ?? undefined,
+    };
+  }
+
+  const result = await httpFetch(input, init);
+
+  const userId = result.headers.get("SNAPLET-USER-ID");
+
+  if (
+    typeof systemConfig.userId === "undefined" &&
+    typeof userId === "string"
+  ) {
+    await updateSystemConfig({ userId });
+  }
+
+  return result;
+};
 
 export const trpc = createTRPCProxyClient<CLIRouter>({
   links: [
@@ -12,6 +49,7 @@ export const trpc = createTRPCProxyClient<CLIRouter>({
         authorization: `Bearer ${await getAccessToken()}`,
         "user-agent": `Snaplet Seed / ${getVersion()}`,
       }),
+      fetch,
     }),
   ],
 });
