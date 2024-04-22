26f3ef31610b43ea106474377741d52aad70fd3f
avallete <andrew@snaplet.dev>
2024-04-23 00:15:25 +0200

    back to 8vcpu

--- packages/seed/e2e/basics/e2e.test.ts
+++ packages/seed/e2e/basics/e2e.test.ts
@@ -37,6 +37,34 @@ for (const [dialect, adapter] of adapterEntries) {
     expect((await db.query('select * from "Member"')).length).toEqual(6);
   });
 
+  test("handle existing data in the database", async () => {
+    const { db } = await setupProject({
+      adapter,
+      databaseSchema: `
+        CREATE TABLE "Organization" (
+          "id" uuid not null primary key
+        );
+        CREATE TABLE "Member" (
+          "id" uuid not null primary key,
+          "organizationId" uuid not null references "Organization"("id"),
+          "name" text not null
+        );
+        INSERT INTO "Organization" VALUES ('18bcdaf9-afae-4b03-a7aa-203491acc950');
+      `,
+      seedScript: `
+        import { createSeedClient } from '#snaplet/seed'
+
+        const seed = await createSeedClient()
+
+        await seed.members((x) => x(5), { connect: { organizations: [{ id: '18bcdaf9-afae-4b03-a7aa-203491acc950' }] } })
+      `,
+    });
+
+    expect((await db.query('select * from "Organization"')).length).toEqual(1);
+
+    expect((await db.query('select * from "Member"')).length).toEqual(5);
+  });
+
   test("dryRun outputs sql statements to stdout", async () => {
     const { db, stdout } = await setupProject({
       adapter,
@@ -72,33 +100,5 @@ for (const [dialect, adapter] of adapterEntries) {
     expect((await db.query('select * from "Organization"')).length).toEqual(2);
 
     expect((await db.query('select * from "Member"')).length).toEqual(6);
-  });
-
-  test("handle existing data in the database", async () => {
-    const { db } = await setupProject({
-      adapter,
-      databaseSchema: `
-        CREATE TABLE "Organization" (
-          "id" uuid not null primary key
-        );
-        CREATE TABLE "Member" (
-          "id" uuid not null primary key,
-          "organizationId" uuid not null references "Organization"("id"),
-          "name" text not null
-        );
-        INSERT INTO "Organization" VALUES ('18bcdaf9-afae-4b03-a7aa-203491acc950');
-      `,
-      seedScript: `
-        import { createSeedClient } from '#snaplet/seed'
-
-        const seed = await createSeedClient()
-
-        await seed.members((x) => x(5), { connect: { organizations: [{ id: '18bcdaf9-afae-4b03-a7aa-203491acc950' }] } })
-      `,
-    });
-
-    expect((await db.query('select * from "Organization"')).length).toEqual(1);
-
-    expect((await db.query('select * from "Member"')).length).toEqual(5);
   });
 }
