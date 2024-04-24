f6d8841de2c021f8c1bca93957992876f8e66bd0
Julien Goux <hi@jgoux.dev>
2024-04-24 16:16:45 +0200

    add type

--- packages/seed/src/adapters/prisma/getPrismaDataModel.ts
+++ packages/seed/src/adapters/prisma/getPrismaDataModel.ts
@@ -1,6 +1,7 @@
 import { getDMMF, getSchema } from "@prisma/internals";
+import { type DMMF } from "@prisma/generator-helper"
 
-export async function getPrismaDataModel() {
+export async function getPrismaDataModel(): Promise<DMMF.Document> {
   const datamodel = await getSchema();
 
   return getDMMF({
