f2e5eaafabfe43c763a444057596410d20ee434d
Julien Goux <hi@jgoux.dev>
2024-04-25 10:50:22 +0200

    fix lint

--- knip.ts
+++ knip.ts
@@ -11,7 +11,9 @@ const optionalPeerDeps = Object.keys(
 
 const config: KnipConfig = {
   ignoreBinaries: ["nix"],
-  ignoreDependencies: optionalPeerDeps.filter((dep) => dep !== "@prisma/client"),
+  ignoreDependencies: optionalPeerDeps.filter(
+    (dep) => dep !== "@prisma/client",
+  ),
   ignore: [
     "./packages/seed/src/index.ts",
     "knip.ts",
