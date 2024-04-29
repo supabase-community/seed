af701a71cfb26fc2346d8c0981cc78176a3e37e0
Justin van der Merwe <justnvdm@gmail.com>
2024-04-29 17:25:29 +0200

    WIP

--- packages/seed/src/cli/commands/init/initHandler.ts
+++ packages/seed/src/cli/commands/init/initHandler.ts
@@ -41,7 +41,10 @@ export async function loggedCommandPrerun(
   };
 }
 
-export async function initHandler(args: { directory: string; reset: boolean }) {
+export async function initHandler(args: {
+  directory: string;
+  reset?: boolean;
+}) {
   // Every files are created in the same directory as the seed.config.ts file
   process.env["SNAPLET_SEED_CONFIG"] = path.join(
     args.directory,
