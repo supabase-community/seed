3ef372e5bb07b09dad55fae5497aa5f27c263b07
Julien Goux <hi@jgoux.dev>
2024-04-29 22:19:54 +0200

    add fallback

--- packages/seed/src/adapters/prisma/getPrismaDataModel.ts
+++ packages/seed/src/adapters/prisma/getPrismaDataModel.ts
@@ -2,7 +2,8 @@ import { type DMMF } from "@prisma/generator-helper";
 
 export async function getPrismaDataModel(): Promise<DMMF.Document> {
   const prismaInternals = await import("@prisma/internals");
-  const { getDMMF, getSchema } = prismaInternals.default;
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  const { getDMMF, getSchema } = prismaInternals.default ?? prismaInternals;
 
   const datamodel = await getSchema();
 
