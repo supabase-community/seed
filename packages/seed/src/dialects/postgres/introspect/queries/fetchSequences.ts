392d32387f391d101077a26e09e9e4fe517e62c6
Andrew Valleteau <avallete@users.noreply.github.com>
2024-04-30 14:23:42 +0200

    feat(core): add global connect option (#153)
    Co-authored-by: Julien Goux <hi@jgoux.dev>

--- packages/seed/src/dialects/postgres/introspect/queries/fetchSequences.ts
+++ packages/seed/src/dialects/postgres/introspect/queries/fetchSequences.ts
@@ -2,11 +2,11 @@ import { type DatabaseClient } from "#core/databaseClient.js";
 import { buildSchemaExclusionClause } from "./utils.js";
 
 interface FetchSequencesResult {
-  current: bigint | number;
   interval: bigint | number;
+  last: null | string;
   name: string;
   schema: string;
-  start: number;
+  start: string;
 }
 
 const FETCH_SEQUENCES = `
@@ -14,7 +14,7 @@ SELECT
   schemaname AS schema,
   sequencename AS name,
   start_value AS start,
-  COALESCE(last_value, start_value) AS current,
+  last_value as last,
   increment_by AS interval
 FROM
  pg_sequences
@@ -24,14 +24,16 @@ WHERE ${buildSchemaExclusionClause("pg_sequences.schemaname")}
 export async function fetchSequences(client: DatabaseClient) {
   const response = await client.query<FetchSequencesResult>(FETCH_SEQUENCES);
 
-  return response.map((r) => ({
-    schema: r.schema,
-    name: r.name,
-    start: Number(r.start),
-    // When a sequence is created, the current value is the start value and is available for use
-    // but when the sequence is used for the first time, the current values is the last used one not available for use
-    // so we increment it by one to get the next available value instead
-    current: r.start === r.current ? Number(r.current) : Number(r.current) + 1,
-    interval: Number(r.interval),
-  }));
+  return response.map((r) => {
+    return {
+      schema: r.schema,
+      name: r.name,
+      start: Number(r.start),
+      // When a sequence is created, the current value is the start value and is available for use
+      // but when the sequence is used for the first time, the current values is the last used one not available for use
+      // so we increment it by one to get the next available value instead
+      current: r.last ? Number(r.last) + 1 : Number(r.start),
+      interval: Number(r.interval),
+    };
+  });
 }
