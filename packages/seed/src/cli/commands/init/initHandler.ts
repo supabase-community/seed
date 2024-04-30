ebc7cfd00ac4bfdb50e1d0bacd6bfc1a0b2019f7
Justin van der Merwe <justnvdm@gmail.com>
2024-04-30 17:27:19 +0200

    Linting fixes

--- packages/seed/src/cli/commands/init/initHandler.ts
+++ packages/seed/src/cli/commands/init/initHandler.ts
@@ -9,10 +9,10 @@ import { bold, highlight } from "../../lib/output.js";
 import { linkHandler } from "../link/linkHandler.js";
 import { loginHandler } from "../login/loginHandler.js";
 import { syncHandler } from "../sync/syncHandler.js";
+import { adapterHandler } from "./adapterHandler.js";
 import { generateSeedScriptExample } from "./generateSeedScriptExample.js";
 import { installDependencies } from "./installDependencies.js";
 import { saveSeedConfig } from "./saveSeedConfig.js";
-import { adapterHandler } from './adapterHandler.js';
 
 export async function initHandler(args: {
   directory: string;
