b90fcde0a725e1f1a1b18eef1a14a90b9fdc4467
Justin van der Merwe <justnvdm@gmail.com>
2024-05-02 16:03:48 +0200

    WIP

--- packages/seed/src/cli/commands/generate/generateHandler.ts
+++ packages/seed/src/cli/commands/generate/generateHandler.ts
@@ -1,23 +1,14 @@
-import { pathToFileURL } from "node:url";
 import { generateAssets } from "#core/codegen/codegen.js";
-import { bold, link, spinner } from "../../lib/output.js";
 import { computeCodegenContext } from "./computeCodegenContext.js";
 
 export async function generateHandler(args: { output?: string }) {
   try {
-    spinner.start(`Generating your ${bold("Seed Client")}`);
-
     const context = await computeCodegenContext({ outputDir: args.output });
 
-    const outputDir = await generateAssets(context);
-
-    spinner.succeed(
-      `Generated your ${bold("Seed Client")} to ${link(pathToFileURL(outputDir).toString())}`,
-    );
+    await generateAssets(context);
 
     return { ok: true };
   } catch (error) {
-    spinner.fail(`Generation failed`);
     throw error as Error;
   }
 }
--- packages/seed/src/cli/commands/init/generateSeedScriptExample.ts
+++ packages/seed/src/cli/commands/init/generateSeedScriptExample.ts
@@ -2,11 +2,9 @@ import dedent from "dedent";
 import { existsSync } from "node:fs";
 import { writeFile } from "node:fs/promises";
 import { dirname, join } from "node:path";
-import { pathToFileURL } from "node:url";
 import { getSeedConfigPath } from "#config/seedConfig/seedConfig.js";
 import { getDataModel } from "#core/dataModel/dataModel.js";
 import { sortModels } from "#core/store/topologicalSort.js";
-import { link, spinner } from "../../lib/output.js";
 
 export async function generateSeedScriptExample() {
   const seedScriptPath = join(dirname(await getSeedConfigPath()), "seed.ts");
@@ -20,7 +18,7 @@ export async function generateSeedScriptExample() {
   const [model] = sortModels(dataModel);
   const template = dedent`
     /**
-     * ! Executing this script will delete all data in your database and seed it with 10 versions.
+     * ! Executing this script will delete all data in your database and seed it with 10 ${model.modelName}.
      * ! Make sure to adjust the script to your needs.
      * Use any TypeScript runner to run this script, for example: \`npx tsx seed.ts\`
      * Learn more about the Seed Client by following our guide: https://docs.snaplet.dev/seed/getting-started
@@ -47,14 +45,6 @@ export async function generateSeedScriptExample() {
   `;
 
   await writeFile(seedScriptPath, template);
-
-  spinner.succeed(
-    `Generated a seed script example to ${link(pathToFileURL(seedScriptPath).toString())}`,
-  );
-
-  spinner.warn(
-    `Executing this script will delete all data in your database and seed it with 10 ${model.modelName}. Make sure to adjust the script to your needs`,
-  );
 
   return seedScriptPath;
 }
