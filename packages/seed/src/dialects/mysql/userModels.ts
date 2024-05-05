4a098eeec22387bdbfca7c57eab31e2a3d4041d7
Andrew Valleteau <avallete@users.noreply.github.com>
2024-05-05 17:26:11 +0200

    fix(mysql): geometry default transforms (#165)

--- packages/seed/src/dialects/mysql/userModels.ts
+++ packages/seed/src/dialects/mysql/userModels.ts
@@ -1,7 +1,25 @@
 import { DEFAULT_SQL_TEMPLATES } from "#core/dialect/userModels.js";
 import { type Templates } from "#core/userModels/templates/types.js";
+import {
+  type MYSQL_GEOMETRY_TYPES,
+  geometryCollection,
+  lineString,
+  multiLineString,
+  multiPoint,
+  multiPolygon,
+  point,
+  polygon,
+} from "./userModels/templates/geometry.js";
 import { type SQLTypeName } from "./utils.js";
 
-export const SQL_TEMPLATES: Templates<SQLTypeName> = {
+export const SQL_TEMPLATES: Templates<MYSQL_GEOMETRY_TYPES | SQLTypeName> = {
   ...DEFAULT_SQL_TEMPLATES,
+  point,
+  geometry: point,
+  linestring: lineString,
+  polygon,
+  multipoint: multiPoint,
+  multilinestring: multiLineString,
+  multipolygon: multiPolygon,
+  geomcollection: geometryCollection,
 };
