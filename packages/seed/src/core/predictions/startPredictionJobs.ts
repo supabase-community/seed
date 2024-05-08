c1637d9a4c45b82f46e8b85fd39e707a4b66f32c
Justin van der Merwe <justnvdm@gmail.com>
2024-05-08 16:27:01 +0200

    Linting

--- packages/seed/src/core/predictions/startPredictionJobs.ts
+++ packages/seed/src/core/predictions/startPredictionJobs.ts
@@ -1,4 +1,4 @@
-import { EventEmitter } from "events";
+import { EventEmitter } from "node:events";
 import { trpc } from "#trpc/client.js";
 import { type PredictionContext } from "./computePredictionContext.js";
 
